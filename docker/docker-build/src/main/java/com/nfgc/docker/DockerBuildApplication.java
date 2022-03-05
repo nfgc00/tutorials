83beadb8ffff05f125febd0f143f2f8d9bb6fd44
Fernando <nfgc00@gmail.com>
2022-03-05 16:22:41 +0100

    docker spring-boot build image added

---
+++ docker/docker-build/src/main/java/com/nfgc/docker/DockerBuildApplication.java
@@ -0,0 +1,13 @@
+package com.nfgc.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBuildApplication.class, args);
+	}
+
+}
